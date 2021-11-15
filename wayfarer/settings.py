1211c1bf89b63463fa37b523b804792113c2b5d9
ovsharam <sharmaapoorva124@gmail.com>
2021-11-15 09:07:15 -0800

    Resolving error key

--- wayfarer/settings.py
+++ wayfarer/settings.py
@@ -15,14 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*sqsjmfhp1whjdwzqq-jbc&%0g%y4$#b9t#^8jn$5e)6enuo_r'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
